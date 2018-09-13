fa8ec91e3fe3ebd85a3da6e075b94c8f6f15f3bf
Nikita Shubin <maquefel@gmail.com>
2018-09-13 13:15:50 +0300

    wrong header guards

--- src/dispatch/gpiod-dispatch-raw.h
+++ src/dispatch/gpiod-dispatch-raw.h
@@ -1,11 +1,10 @@
-#ifndef __GPIOD_UAPI_H__
-#define __GPIOD_UAPI_H__
+#ifndef __GPIOD_DISPATCH_RAW_H__
+#define __GPIOD_DISPATCH_RAW_H__
 
 #include <stddef.h>
 #include <stdint.h>
 #include <sys/time.h>
 
 size_t dispatch_raw_pack(char** /*data*/, struct timeval /*ts*/, uint8_t /*chan*/, uint8_t /*value*/);
-
 
 #endif
